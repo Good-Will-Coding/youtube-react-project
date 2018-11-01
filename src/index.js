7c92e14d44e3121b13a074ea9f27d42a16c6b77d
billy <billyliadphillips@gmail.com>
2018-11-01 11:58:49 +0000

    updated video list items

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+const API_KEY = "AIzaSyAMvqbRqkXA_fBIDOCtV-ng99siZXfkRzs";
+
+// Create a new component Should produce HTML
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "surfboards" },
+    videos => this.setState({ videos }));
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} /> 
+      </div>
+    );
+  }
+}
+
+// Take this component generated HTML and put it on
+// page
+
+ReactDOM.render(<App />, document.querySelector(".container"));
