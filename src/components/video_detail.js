6e62bf82bd2d222a276aa9244aa914013349265d
billy <billyliadphillips@gmail.com>
2018-11-02 09:16:01 +0000

    Added video detail components

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const VideoDetail = ({ video }) => {
     const videoID = video.id.videoID;
-    const url = "https://www.youtube.com/embed/" + videoID;
-    
+    const url = `https://www.youtube.com/embed/${videoID}`;
+
     return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" />
+        <iframe className="embed-responsive-item" src={url}/>
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyAMvqbRqkXA_fBIDOCtV-ng99siZXfkRzs";
 
 // Create a new component Should produce HTML
